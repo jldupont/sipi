5aeb1c43ffbc7b35203f740d562eb9a096c8e852
jldupont <github@jldupont.com>
2012-02-01 06:40:20 -0500

    added "atomic_write"

--- src/sipi/tools_os.py
+++ src/sipi/tools_os.py
@@ -4,8 +4,41 @@
 """
 import os, errno
 import shutil
+import tempfile
+
+def atomic_write(path, contents):
+    """
+    Atomic write to file
+    
+    Create temporary file and then move/rename to specified path.
+    Rename operation in the same filesystem are atomic (at least in Linux).
+    
+    >>> atomic_write("/tmp/_jlddk_atomic_write", "test!")
+    """
+    fd, tfn=tempfile.mkstemp()
+    
+    try:
+        ### part 1: write to temp file
+        f=os.fdopen(fd, "w")
+        f.write(contents)
+        f.close()
+    except Exception, e:
+        try:    os.close(fd)
+        except: pass
+        return ("error", "write to temp file: %s" % str(e))        
+        
+    try:
+        ### part 2: rename to specified path
+        os.rename(tfn, path)
+    except:
+        return ("error", "rename to path '%s'" % path)
+        
+    return ("ok", tfn)
 
 def move(src_path, dst_path):
+    """
+    Move file
+    """
     try:
         shutil.move(src_path, dst_path)
         return ("ok", None)
